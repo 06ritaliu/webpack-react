3968668e6dad8c80014e04e2d8e99be1a38b8100
LiuTing <tliu42@best-inc.com>
2020-01-03 17:31:32 +0800

    init

---
+++ src/entry/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import './style.css';
+import App from './App';
+
+const renderDom = Component => {
+  render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app')
+  );
+}
+renderDom(App);
+
+if (module.hot) {
+  module.hot.accept('./App.js', () => {
+    console.log('Accepting the updated printMe module!');
+    renderDom(App);
+  })
+}
---
+++ webpack/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const rootDir = path.resolve(__dirname, '../');
+const srcDir = path.resolve(rootDir, './src');
+const webDir = path.resolve(rootDir, './web');
+const nodeModuleDir = path.resolve(rootDir, './node_modules');
+
+var jsPath                        = 'res/js/';
+var cssPath                       = 'res/css/';
+var imgPath                       = 'res/img/';
+var fontsPath                     = 'res/fonts/';
+
+module.exports = {
+    target: "web",
+    entry: {
+        app: ['react-hot-loader/patch', path.resolve(srcDir, './entry/index.js')]
+    },
+    output: {
+        path: webDir,
+        filename: jsPath + '[name].bundle.js',
+        chunkFilename: jsPath + 'chunk_[id].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: nodeModuleDir
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: ["babel-loader"],
+                exclude: nodeModuleDir
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+            template: path.resolve(srcDir, './template/index.ejs')
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    // watch: true,
+    devServer: {
+        contentBase: webDir,
+        port: 3000,
+        open: true,
+        hot: true,
+        host: "localhost",
+        hotOnly: true,
+        inline: true,
+        publicPath: '/'
+    }
+};
