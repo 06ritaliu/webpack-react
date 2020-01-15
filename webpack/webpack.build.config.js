41be2daae25ecf3861feff825e765ab51bf5044a
balconyLiu <liuting_jndx@163.com>
2020-01-15 16:21:29 +0800

    添加生产配置文件和静态资源

--- webpack/webpack.build.config.js
+++ webpack/webpack.build.config.js
@@ -1,14 +1,15 @@
-/**本地开发调试时的webpack配置文件（支持HMR和react的react-hot-loader）
- * webpack dev-server打包的文件目录是web文件夹，但实际文件存储在内存中不可见
+/**生产项目使用的webpack配置文件
+ * 最终打包生成的文件放置在websrc文件夹下
+ * 本地打开会报错，原因是html文件中自动生成的bundle.js文件是绝对路径，适用于服务器端部署情况。
+ * 若需在本地通过查看文件方式直接打开预览，可手动修改为相对路径
  */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //每次打包成功构建之前，删除指定文件夹内文件，保留静态加载文件
-const webpack = require('webpack');
 
 const rootDir = path.resolve(__dirname, '../');
 const srcDir = path.resolve(rootDir, './src');
-const webDir = path.resolve(rootDir, './web');
+const webDir = path.resolve(rootDir, './websrc');
 const nodeModuleDir = path.resolve(rootDir, './node_modules');
 
 var jsPath                        = 'res/js/';
@@ -19,11 +20,7 @@ var fontsPath                     = 'res/fonts/';
 module.exports = {
     target: "web",
     entry: {
-        app: [
-            'react-hot-loader/patch',
-            'webpack/hot/only-dev-server',
-            path.resolve(srcDir, './entry/index.js')
-        ]
+        app: path.resolve(srcDir, './entry/index.js')
     },
     output: {
         path: webDir,
@@ -46,25 +43,13 @@ module.exports = {
         ]
     },
     plugins: [
+        //在最终的html中自动插入所有生成的bundle文件，默认插入在body末端。
         new HtmlWebpackPlugin({
             title: "Output Management",
             template: path.resolve(srcDir, './template/index.ejs')
         }),
         new CleanWebpackPlugin({
             cleanOnceBeforeBuildPatterns: ['res/*']
-        }),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
+        }), // 这个插件的路径是相对于项目root来取的，而不是相对于当前文件
     ],
-    // watch: true, //watch模式
-    devServer: { //dev-server模式
-        contentBase: webDir,
-        port: 3000,
-        open: true,
-        hot: true,
-        host: "localhost",
-        hotOnly: true,
-        inline: true,
-        publicPath: '/'
-    }
 };
